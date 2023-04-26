ee6c5be4b0352986ec2862742673f28628975452
DaianaM <daiana.m.0708@gmail.com>
2023-04-26 17:48:21 -0400

    Initial Commit

---
+++ QuestionsApp/QuestionsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionsAppApp.swift
+//  QuestionsApp
+//
+//  Created by scholar on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct QuestionsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
